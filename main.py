b4e2f2c33d3ff688ba2d980beb9f867bb09cee30
reysanchez <reysanchez@users.noreply.github.com>
2025-10-23 09:43:12 -0500

    guardar

---
+++ main.py
@@ -0,0 +1,6 @@
+from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.textinput import TextInput
+from kivy.uix.button import Button
+from kivy.uix.gridlayout import GridLayout
+#xgxxfrom kivy.uix.core.window import Window
